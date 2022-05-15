b4caabd618ad3590dc36347d273b9986eb5fbea9
黄之鸣 <zhiminghuang@uvic.ca>
2022-05-14 20:58:44 -0700

    Update errctl_sim.py

--- errctl_sim.py
+++ errctl_sim.py
@@ -10,7 +10,7 @@ import numpy as np
 class Errctl_Sim:
     def __init__(self, tracefile="starwars.frames.old"):
         # read the tracefile
-        tracefile = open(tracefile, "r+")
-        traces = tracefile.read().splitlines()[0:10000]
-        traces = np.array(list(map(int, traces)))
-        tracefile.close()
+        self.tracefile = open(tracefile, "r+")
+        self.traces = tracefile.read().splitlines()[0:10000]
+        self.traces = np.array(list(map(int, self.traces)))
+        self.tracefile.close()
